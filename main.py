b651b5a6953010f6cd3c0dde62117fc1ff83c9bf
ashutosh <ak956901@gmail.com>
2024-06-21 14:25:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import time as t
+import cohere
+
+st.title("Chat Bot")
+cont=st.container()
+
+##Setting api key
+co=cohere.Client(api_key=st.secrets["COHERE_API_KEY"])
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[]
+
+with cont:
+    for m in st.session_state["messages"]:
+        if m["role"]=='user':
+            st.chat_message("user").write(m["content"])
+        if m["role"]=="assistant":
+            st.chat_message("assistant").write(m["content"])
+
+prompt=st.chat_input("Enter your message:")
+
+if prompt:
+    st.session_state["messages"].append({
+        "role":"user",
+        "content":prompt
+    })
+    st.chat_message("user").write(prompt)
+
+    stream=co.chat_stream(message=prompt)
+    
+    asst=st.chat_message("assistant")
+    res_cont=asst.empty()
+
+    res=""
+
+    for event in stream:
+        if event.event_type=="text-generation":
+            res+=event.text
+            res_cont.write(res)
+            t.sleep(0.02)
+
+    st.session_state["messages"].append({
+        "role":"assistant",
+        "content":res 
+    })
+
+   
+ 
